775d867e5a541add223f386c55ca77b64c0e768c
MD10-25 <martindimitrov04@gmail.com>
2022-11-28 13:08:59 +0100

    Orange knap er lavet færdig

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import OrangeBtn from './Components/OrangeBtn';
 import Header from './Templates/Header';
 function App() {
   return (
     <div>
   <Header/>
+  
     </div>
   );
 }
---
+++ src/Components/OrangeBtn.js
@@ -0,0 +1,15 @@
+import './orangeBtn.css'
+const OranegBtn = ({text}) => {
+    
+    return ( 
+    <>
+        <button className='orangeBtn'>{text}</button>
+    </>
+
+     );
+}
+ 
+export default OranegBtn;
+
+
+//  Når knappen skal implimenteres i fx App.js <OrangeBtn text="Read more"/> 

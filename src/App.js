815c064999916deb619cc202cfd903ec2c4cc7d1
MD10-25 <martindimitrov04@gmail.com>
2022-11-28 13:11:54 +0100

    orange knap er lavet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import OrangeBtn from './Components/OrangeBtn';
 import Header from './Templates/Header';
 function App() {
   return (
     <div>
   <Header/>
-  
+
     </div>
   );
 }

9b8a96c3ddd912fc37b3b862c5b4b7c6e03fab7b
MD10-25 <martindimitrov04@gmail.com>
2022-11-28 14:15:11 +0100

    Routes og Outlet oprettet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, createRoutesFromElements,
+  RouterProvider, Route } from 'react-router-dom';
+
+
+
+  export const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Route>
+    )
+  );
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router}/>
   </React.StrictMode>
 );
 

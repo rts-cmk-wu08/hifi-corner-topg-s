30265ce87bf30591c81c3b43161a3bdf14ef6a3d
MD10-25 <martindimitrov04@gmail.com>
2022-11-29 10:44:39 +0100

    added routing og pages

---
+++ src/Pages/Products.js
@@ -0,0 +1,17 @@
+
+
+import Cards from '../Components/Card';
+
+
+
+const Products = () => {
+    return (
+        <> 
+        <h1>Dette er Products</h1>
+
+        <Cards/>
+        </>
+     );
+}
+ 
+export default Products;

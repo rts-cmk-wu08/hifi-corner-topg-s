35f3069d8fac0ec5d33f76d43278b6cd2838dcdd
djbenjamin2001 <benjaminbatbayli@gmail.com>
2022-12-01 10:56:48 +0100

    counter is made and app.js has no changes

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
-
+import OrangeBtn from "./OrangeBtn"
 //components/Counter.js
 
 
-const Counter = () => {
+const Counter = (props) => {
 
-const [ count, setCount] = useState(0); 
+const [ count, setCount] = useState(parseInt(localStorage.getItem(props.itemname))  || 0); 
 
 useEffect(() => {
     console.log("hello from Counter Useffect!")
+    localStorage.setItem(props.itemname, count)
 
-}, [count]);
+}, [count, props.itemname]);
 
 const handleIncrease = () => { if (count < 10) setCount(count + 1)}
    
@@ -24,7 +25,7 @@ const handleDecrease = () => { if (count > 0) setCount(count - 1)}
   <p>{count}</p>
     <button onClick={handleIncrease}>+</button>
     <button onClick={handleDecrease}>-</button>
-    
+    <OrangeBtn text="add to cart"/>
       </> 
      );
 }

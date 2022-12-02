13565d32188a1617bae8b51a60b17d5c980b8290
djbenjamin2001 <benjaminbatbayli@gmail.com>
2022-12-02 13:30:05 +0100

    Cart componented er lavet

---
+++ src/Components/Cart.js
@@ -0,0 +1,18 @@
+import {FaShoppingCart} from "react-icons/fa"
+import { useState } from "react";
+import "./Cart.css"
+const Cart = () => {
+
+    const [dropdownVisible, setDropdownVisible] = useState(false)
+    const handleClick =() => {setDropdownVisible(!dropdownVisible)}
+    console.log(dropdownVisible)
+    return (  
+        <section className="Cart">
+<FaShoppingCart onClick={handleClick} size={35} />
+<div className={`cartContents ${dropdownVisible ? 'cartContents--visible' : ''}`}>
+blabla
+</div></section>
+    );
+}
+ 
+export default Cart;

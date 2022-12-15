9a0a8b8db703d42856e3cd9a7a5c583ec002303d
Peter <peter-lauridsen@live.dk>
2022-12-15 08:55:44 +0100

    adasd

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,48 +1,86 @@
-import {FaShoppingCart} from "react-icons/fa"
+import { FaShoppingCart } from "react-icons/fa";
 import { useState, useContext } from "react";
-import "./Cart.css"
+import "./Cart.css";
 import OranegBtn from "./OrangeBtn";
 
 import { Cartcontext } from "../Contexts/Cartcontext";
 const Cart = () => {
-const { cartCount, cartItems, totalPrice,handleRemoveFromCart,handleDecrease,handleIncrease } = useContext(Cartcontext)
-    const [dropdownVisible, setDropdownVisible] = useState(false)
-    const handleClick =() => {setDropdownVisible(!dropdownVisible)
-    console.log("hello")
-    }
-    
-    console.log(dropdownVisible)
-   
-    return (  
-        <section className="Cart">
-<FaShoppingCart onClick={handleClick} size={35} />
-<div className={`cartContents ${dropdownVisible ? 'cartContents--visible' : ''}`}>  
-<h2>Cart</h2>
- {cartItems.map((item) => (
-    <div className="cartItem">
-        <button onClick={()=>{handleRemoveFromCart(item.id)}}>X</button>
-        <img src={item.imageUrl} alt=""/>
-        <div className="cartItem__info">
-            <h3>{item.name}</h3>
-            <p>Price: {    Intl.NumberFormat('en-GB', {style: 'currency', currency: 'GBP'})
-    .format(item.price)}</p>
-            <p>Quantity: {item.count}</p>
-        </div>
-        <button onClick={()=>{handleIncrease(item.id)}}>+</button>
-        <button onClick={()=>{handleDecrease(item.id)}}>-</button>
-    </div>
-))}
-<p>total items : { cartCount }</p>
-<p>total price : { Intl.NumberFormat('en-GB', {style: 'currency', currency: 'GBP'})
-    .format(totalPrice) }</p>
+  const {
+    cartCount,
+    cartItems,
+    totalPrice,
+    handleRemoveFromCart,
+    handleDecrease,
+    handleIncrease,
+  } = useContext(Cartcontext);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const handleClick = () => {
+    setDropdownVisible(!dropdownVisible);
+    console.log("hello");
+  };
 
-<OranegBtn text="Go to cart"/>
-<OranegBtn text="Go to Payments"/>
-</div>
+  console.log(dropdownVisible);
 
+  return (
+    <section className="Cart">
+      <FaShoppingCart onClick={handleClick} size={35} />
+      <div
+        className={`cartContents ${
+          dropdownVisible ? "cartContents--visible" : ""
+        }`}
+      >
+        <h2>Cart</h2>
+        {cartItems.map((item) => (
+          <div className="cartItem">
+            <button
+              onClick={() => {
+                handleRemoveFromCart(item.id);
+              }}
+            >
+              X
+            </button>
+            <img src={item.imageUrl} alt="" />
+            <div className="cartItem__info">
+              <h3>{item.name}</h3>
+              <p>
+                Price:{" "}
+                {Intl.NumberFormat("en-GB", {
+                  style: "currency",
+                  currency: "GBP",
+                }).format(item.price)}
+              </p>
+              <p>Quantity: {item.count}</p>
+            </div>
+            <button
+              onClick={() => {
+                handleIncrease(item.id);
+              }}
+            >
+              +
+            </button>
+            <button
+              onClick={() => {
+                handleDecrease(item.id);
+              }}
+            >
+              -
+            </button>
+          </div>
+        ))}
+        <p>total items : {cartCount}</p>
+        <p>
+          total price :{" "}
+          {Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP",
+          }).format(totalPrice)}
+        </p>
 
-</section>
-    );
-}
- 
+        <OranegBtn text="Go to cart" />
+        <OranegBtn text="Go to Payments" />
+      </div>
+    </section>
+  );
+};
+
 export default Cart;

96d2a3dfc9e11dcc094632a0bd3c2f50c28a68dc
Peter <peter-lauridsen@live.dk>
2022-12-16 14:07:48 +0100

    Added text

--- src/Pages/FAQ.js
+++ src/Pages/FAQ.js
@@ -95,30 +95,50 @@ const faq = () => {
           </p>
         </article>
         <article>
-          <h2>Refunds</h2>
+          <h2>Returns process/your statutory rights</h2>
           <p>
-            Once your returned item is received and inspected, we will send you
-            an email or phone call to notify you that we have received your
-            returned item.We will also notify you of the approval or rejection
-            of your refund. If you are approved, then your refund will be
-            processed, and a credit will automatically be applied to your
-            original method of payment, within 3 working days. Please note we
-            require an email address or contact number to arrange a refund that
-            was purchased over the phone. There are certain situations where
-            only partial refunds are granted or we cannot refund you; Any item
-            not in its original condition, is damaged or missing parts for
-            reasons not due to our error. Any damage that has been caused by
-            misuse, such as a speaker drive unit being blown or water being
-            introduced to the unit, or that has been damaged due to poor
-            packing. Any item that is returned more than 30 days after delivery.
-            In any instance, we will notify you as soon as possible to resolve
-            this matter. If you haven’t received a refund after confirmation,
-            first check your bank account again. Then contact your credit card
-            company, it may take some time before your refund is officially
-            posted. Next contact your bank. There is often some processing time
-            of a couple of days before a refund is posted. If you have done all
-            of this and you still have not received your refund after 7 days,
-            please contact us at sales@hificorner.co.uk.
+            If you wish to return goods purchased online or over the phone, you
+            should notify Hi-Fi Corner of your intention to do so within 14 days
+            of the date of receipt. You then have an additional 14 days to
+            return the goods from this notification date (goods must be returned
+            within 28 days). We will in such circumstances refund you the cost
+            of your order, excluding postage or delivery cost. However, if goods
+            are not returned "as new" due to excessive handling on your part, or
+            with components missing (including packing materials and manuals) we
+            may make a reasonable deduction for this. Please do not send your
+            product back to the manufacturer. We kindly ask that to make the
+            refund process easier, that you include your receipt or proof of
+            purchase and a cover letter detailing briefly why the item is being
+            returned. We cannot refund the cost of delivery fees if there was an
+            additional charge, only the cost of the goods themselves. We will
+            process the refund when the item arrives with us. If you are
+            shipping an item over £100, we would advise using a trackable
+            shipping service or purchasing shipping insurance. We cannot
+            guarantee that we will receive your returned item. Please contact us
+            before attempting to return your goods. For purchases bought
+            in-store, you can also drop in and be refunded instantly.
+          </p>
+        </article>
+        <article>
+          <h2>Sale items</h2>
+          <p>
+            Only regularly priced items may be refunded, unfortunately
+            clearance, special order and outlet items cannot be refunded unless
+            they are found to be faulty on arrival. For more details about
+            returning secondhand or sale items, please contact
+            sales@hificorner.co.uk.
+          </p>
+        </article>
+        <article>
+          <h2>Exchanges</h2>
+          <p>
+            We can only replace items if they are defective or damaged. If you
+            need to exchange the product for the same item due to a fault,
+            please first send us an email at sales@hificorner.co.uk and once
+            approved, send your item to: Hi-Fi Corner, 2 Joppa Road, Mackenzie
+            House, Portobello, Edinburgh, EH15 2EU. Please also include the
+            proof of purchase and a cover letter detailing the reason for
+            exchange.
           </p>
         </article>
       </div>

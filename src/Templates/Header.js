9b3096880486ab42a83a2b26fcc3683d80e04317
ThomasKristensen05 <thomas12andre@gmail.com>
2022-12-16 14:10:32 +0100

    header næten færdig

--- src/Templates/Header.js
+++ src/Templates/Header.js
@@ -1,11 +1,15 @@
 import Navigation from "../Components/Navigation";
 import SearchBar from "../Components/SearchBar";
+import { FaUser, FaShoppingCart, FaSearch } from "react-icons/fa"
 import "./Header.css"
 const Header = () => {
     return ( 
         <header>
             <Navigation header="true" logo="true"></Navigation>
             <SearchBar/>
+            <FaUser className="user-icon"/>
+            <FaShoppingCart className="cart-icon"/>
+            <FaSearch className="search-icon" />
         </header>
      );
 }

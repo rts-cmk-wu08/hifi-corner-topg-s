f7d48a9d362e268259b383dbdbe61785293780c2
Peter <peter-lauridsen@live.dk>
2022-12-16 14:03:22 +0100

    Added css and faq.js

---
+++ src/Pages/FAQ.js
@@ -0,0 +1,129 @@
+import "./FAQ.css";
+
+const faq = () => {
+  return (
+    <section className="FAQ">
+      <h1>FAQ</h1>
+      <div className="FAQ__wrapper">
+        <p>
+          Here at Hi-Fi Corner, our trained product specialists aim to provide
+          the right product for you. We take great care with all of our orders
+          to ensure you are fully happy with the product.{" "}
+        </p>
+        <article>
+          We aim to resolve any problems to your satisfaction. Please contact us
+          directly, if, for any reason, you have an issue with your order, your
+          product or any other service provided. OUR GUARANTEE All items
+          excluding ex-display and secondhand goods are covered by at least 12
+          months warranty; this is detailed on your receipt. Any secondhand or
+          ex-display product is covered by at least 3 months warranty as noted
+          in the original listing. Hi-Fi Corner cannot guarantee against any
+          misuse (i.e. blown speakers etc.), commercial use, wear and tear,
+          erosion of parts, loss of data stored on any form of writeable/hard
+          drive devices, dead pixels of an amount not covered by the
+          manufacturer’s specifications or where you have not allowed repairs of
+          goods to be carried out by authorised agents. If it has been more than
+          30 days since you have received a product and discovered a fault, we
+          will still repair the goods free of charge or, if you prefer, replace
+          them provided that you report the fault within 12 months of purchase.
+          If we are unable to repair or replace the item(s) we will provide you
+          with a full refund instead. Please note that if it is after the
+          initial 30 days of purchase you must be able to certify the fault was
+          present upon delivery and not by misuse.
+        </article>
+        <article>
+          <h2>Refunds</h2>
+          <p>
+            Once your returned item is received and inspected, we will send you
+            an email or phone call to notify you that we have received your
+            returned item.We will also notify you of the approval or rejection
+            of your refund. If you are approved, then your refund will be
+            processed, and a credit will automatically be applied to your
+            original method of payment, within 3 working days. Please note we
+            require an email address or contact number to arrange a refund that
+            was purchased over the phone. There are certain situations where
+            only partial refunds are granted or we cannot refund you; Any item
+            not in its original condition, is damaged or missing parts for
+            reasons not due to our error. Any damage that has been caused by
+            misuse, such as a speaker drive unit being blown or water being
+            introduced to the unit, or that has been damaged due to poor
+            packing. Any item that is returned more than 30 days after delivery.
+            In any instance, we will notify you as soon as possible to resolve
+            this matter. If you haven’t received a refund after confirmation,
+            first check your bank account again. Then contact your credit card
+            company, it may take some time before your refund is officially
+            posted. Next contact your bank. There is often some processing time
+            of a couple of days before a refund is posted. If you have done all
+            of this and you still have not received your refund after 7 days,
+            please contact us at sales@hificorner.co.uk.
+          </p>
+        </article>
+        <article>
+          <h2>Shipping</h2>
+          <p>
+            To return your product, you should mail your product to: 2 Joppa
+            Road, Mackenzie House, Edinburgh, EH15 2EU. If you would like to
+            return an item, please note that we cannot arrange a collection of
+            the item without additional cost; you will be responsible for
+            ensuring the product reaches us. Shipping costs are non-refundable.
+            If you receive a refund, the cost of return shipping will be
+            deducted from your refund. Depending on where you live, the time it
+            may take for your exchanged product to reach you may vary. We will
+            process the refund when the item arrives with us. If you are
+            shipping an item over £100, we would advise using a trackable
+            shipping service or purchasing shipping insurance. We cannot
+            guarantee that we will receive your returned item.
+          </p>
+        </article>
+        <article>
+          <h2>Delivery</h2>
+          <p>
+            Please check the condition of all products delivered as soon as they
+            are received. This does not affect your statutory rights. If there
+            is a problem we would like to deal with this as soon as possible to
+            alleviate any inconvenience to yourself. As soon as we have
+            delivered the goods to you, you will be responsible for them and any
+            damage occasioned whilst in your possession. The majority of goods
+            we send will require you to sign for them upon delivery. If you are
+            not able to sign for a delivery, we will do our utmost to contact
+            you to arrange when it is convenient to redeliver. Our
+            responsibility for everything other than damage due to our
+            negligence will end at the time the carrier tried to delivery the
+            goods to you. If your product is visibly damaged in transit, please
+            sign for the delivery as ‘damaged’ and let us know as soon as
+            possible.
+          </p>
+        </article>
+        <article>
+          <h2>Refunds</h2>
+          <p>
+            Once your returned item is received and inspected, we will send you
+            an email or phone call to notify you that we have received your
+            returned item.We will also notify you of the approval or rejection
+            of your refund. If you are approved, then your refund will be
+            processed, and a credit will automatically be applied to your
+            original method of payment, within 3 working days. Please note we
+            require an email address or contact number to arrange a refund that
+            was purchased over the phone. There are certain situations where
+            only partial refunds are granted or we cannot refund you; Any item
+            not in its original condition, is damaged or missing parts for
+            reasons not due to our error. Any damage that has been caused by
+            misuse, such as a speaker drive unit being blown or water being
+            introduced to the unit, or that has been damaged due to poor
+            packing. Any item that is returned more than 30 days after delivery.
+            In any instance, we will notify you as soon as possible to resolve
+            this matter. If you haven’t received a refund after confirmation,
+            first check your bank account again. Then contact your credit card
+            company, it may take some time before your refund is officially
+            posted. Next contact your bank. There is often some processing time
+            of a couple of days before a refund is posted. If you have done all
+            of this and you still have not received your refund after 7 days,
+            please contact us at sales@hificorner.co.uk.
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default faq;

22003befce4f49de698702e45632eb669f034a93
ThomasKristensen05 <thomas12andre@gmail.com>
2022-12-19 13:41:21 +0100

    header nogenlunde færdig

--- src/Templates/Header.js
+++ src/Templates/Header.js
@@ -1,15 +1,20 @@
 import Navigation from "../Components/Navigation";
 import SearchBar from "../Components/SearchBar";
 import { FaUser, FaShoppingCart, FaSearch } from "react-icons/fa"
+import { Link } from "react-router-dom";
 import "./Header.css"
 const Header = () => {
     return ( 
         <header>
             <Navigation header="true" logo="true"></Navigation>
-            <SearchBar/>
-            <FaUser className="user-icon"/>
-            <FaShoppingCart className="cart-icon"/>
-            <FaSearch className="search-icon" />
+                <SearchBar/>
+                <Link to="#">
+                    <FaShoppingCart className="cart-icon"/>
+                </Link>
+                <Link to="#">
+                    <FaUser className="user-icon"/>
+                </Link>
+                <FaSearch className="search-icon" />
         </header>
      );
 }

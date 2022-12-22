52ba247014271aee0d49222f619dd619a2cbdf8e
Peter <peter-lauridsen@live.dk>
2022-12-22 08:54:49 +0100

    ændret rækkefølge på nav

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -9,8 +9,8 @@ const Navigation = ({header,logo}) => {
         <nav>
           {logo &&  "true" ?  <Link  to='/'><img src={logoImgsml} alt="" /></Link> : <Link className="Linked--color" to='/'><p>Home</p></Link>}
          
-          <Link className="Linked--color" to='/FAQ'><p>About Us</p></Link>
           <Link className="Linked--color" to='/products'><p>Shop</p></Link>
+          <Link className="Linked--color" to='/FAQ'><p>About Us</p></Link>
 
           {header && (
             <Link className="contactUs" to="/Contact"><p>Contact Us</p></Link>
